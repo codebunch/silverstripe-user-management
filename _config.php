153590832491fceccdfc18548f41070a91141bf4
codebunch <veluchamysathishkumar@gmail.com>
2019-01-30 22:05:53 +1300

    Bug fix in config

--- _config.php
+++ _config.php
@@ -1,10 +1,13 @@
 <?php
-$config = SilverStripe\SiteConfig\SiteConfig::current_site_config();
-$lost_password_url_segment = $config->LostPasswordUrl()->URLSegment;
-if ($lost_password_url_segment) {
-    SilverStripe\Security\Security::config()->set('lost_password_url', $lost_password_url_segment);
-}
-$login_url_segment = $config->LoginUrl()->URLSegment;
-if ($login_url_segment) {
-    SilverStripe\Security\Security::config()->set('login_url', $login_url_segment);
+
+if (SilverStripe\ORM\DB::is_active() && count(SilverStripe\ORM\DB::table_list()) > 0) {
+    $config = SilverStripe\SiteConfig\SiteConfig::current_site_config();
+    $lost_password_url_segment = $config->LostPasswordUrl()->URLSegment;
+    if ($lost_password_url_segment) {
+        SilverStripe\Security\Security::config()->set('lost_password_url', $lost_password_url_segment);
+    }
+    $login_url_segment = $config->LoginUrl()->URLSegment;
+    if ($login_url_segment) {
+        SilverStripe\Security\Security::config()->set('login_url', $login_url_segment);
+    }
 }
