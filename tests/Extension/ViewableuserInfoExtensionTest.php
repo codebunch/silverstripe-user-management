69b2a459cdf987cedb2735252bac2fc862b38fd0
Sathish <46102368+codebunch@users.noreply.github.com>
2019-02-09 00:13:35 +1300

    Update ViewableuserInfoExtensionTest.php

--- tests/Extension/ViewableuserInfoExtensionTest.php
+++ tests/Extension/ViewableuserInfoExtensionTest.php
@@ -32,7 +32,7 @@ class ViewableuserInfoExtensionTest extends FunctionalTest
         if ($data) {
             $this->markTestIncomplete('Login page exists');
         } else {
-            $this->assertInternalType('false', $data);
+            $this->assertInternalType('bool', $data);
         }
     }
 }
