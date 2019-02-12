f1341d9e8293d6b0da454a1c541c56635dd037f6
codebunch <veluchamysathishkumar@gmail.com>
2019-02-12 22:39:58 +1300

    fixture fix

--- tests/Extension/LoginHandlerExtensionTest.php
+++ tests/Extension/LoginHandlerExtensionTest.php
@@ -2,7 +2,7 @@
 
 namespace UserManagement\Tests;
 
-use SilverStripe\Dev\FunctionalTest;
+use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Security\Security;
 use UserManagement\Extension\LoginHandlerExtension;
 use SilverStripe\Security\Member;
@@ -12,13 +12,18 @@ use SilverStripe\Security\Member;
  *
  * @package user-management
  */
-class LoginHandlerExtensionTest extends FunctionalTest
+class LoginHandlerExtensionTest extends SapphireTest
 {
+    public static $fixture_file = array(
+        __DIR__ . '/../Fixtures/user.yml'
+    );
     /**
      * Login redirect
      */
     public function testRedirect()
     {
+        $member = $this->objFromFixture(Member::class, "joebloggs");
+        $this->logInAs($member);
         $this->markTestIncomplete('Test user config CMS fields');
     }
 }
